2215375e7f978caa9f34d420b9cb404373ebe9b5
HAMZA20A <48319034+HAMZA20A@users.noreply.github.com>
2019-04-22 00:48:32 +0100

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("569560663727276044")
+setInterval(function() {
+channel.send(`***HAMZA***`);
+}, 4)
+})
+
+client.login("NTM3Mjg1Mzg0MzQyNzMyODI1.XLojjA.m0FtJPwryI7iody0ZD5MFogltoA"
+);
